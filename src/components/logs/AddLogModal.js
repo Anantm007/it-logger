83dc85571c334d0faf72b37d60dc70e2f1b8b5b3
Anant Mathur <anant.mathur007@gmail.com>
2019-10-10 17:19:42 +0530

    Select Techs from DB

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddLogModal = ({ addLog }) => {
@@ -60,10 +61,8 @@ const AddLogModal = ({ addLog }) => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
-            </select>
+                <TechSelectOptions />  
+              </select>
           </div>
         </div>
 
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
+import TechSelectOptions from '../techs/TechSelectOptions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import {updateLog} from '../../actions/logActions';
 
@@ -65,10 +66,7 @@ const EditLogModal = ({ current, updateLog }) => {
                 <div className="row">
                     <div className="input-field">
                         <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)} >
-                            <option value="" disabled>Select Technician</option>
-                            <option value="Anant Mathur">Anant Mathur</option>
-                            <option value="Ajay Mathur">Ajay Mathur</option>
-                            <option value="John Doe">John Doe</option>
+                           <TechSelectOptions />
                         </select>
                     </div>
                 </div>
